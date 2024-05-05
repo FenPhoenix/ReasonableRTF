895aecaa791ba1f7004629db570b14451b3d7834
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-05-04 17:12:38 -0700

    Add test app

---
+++ ReasonableRTF_TestApp/MainForm.cs
@@ -0,0 +1,14 @@
+namespace ReasonableRTF_TestApp;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
