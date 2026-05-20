080d63c82b22a24362bd4fcbdbe7894bac940887
FenPhoenix <FenPhoenix@users.noreply.github.com>
2026-05-20 14:57:17 -0700

    Move plaintext array to separate file

--- ReasonableRTF/RtfToTextConverter.cs
+++ ReasonableRTF/RtfToTextConverter.cs
@@ -108,7 +108,6 @@ public sealed partial class RtfToTextConverter
     private const int _parMaxLength = 5;
     private readonly uint _parUInt = BitConverter.IsLittleEndian ? 0x7261705Cu : 0x5C706172u;
 
-    private const int _plainTextDefaultCapacity = 4096;
     private const int _internalBufferDefaultCapacity = 32;
     // 20 bytes * 4 for up to 4 bytes per char. Chars are 2 bytes but like whatever, why do math when you can
     // over-provision.
@@ -1970,8 +1969,6 @@ public sealed partial class RtfToTextConverter
         _windows1252Encoding = Encoding.GetEncoding(_windows1252);
 
         InitSymbolFontData();
-
-        _plainText = new char[_plainTextDefaultCapacity];
 
         _fontDictionary = new Dictionary<int, FontEntry>(_internalBufferDefaultCapacity);
 
@@ -5341,78 +5338,6 @@ public sealed partial class RtfToTextConverter
 #else
         _fontDictionary = new Dictionary<int, FontEntry>(capacity);
 #endif
-    }
-
-    #endregion
-
-    #region PlainText array
-
-    private char[] _plainText;
-    private int _plainText_Capacity;
-
-    private int _plainText_Count;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void PlainText_Add(char item)
-    {
-        if (_plainText_Count == _plainText_Capacity) PlainText_EnsureCapacity(_plainText_Count + 1);
-        _plainText[_plainText_Count++] = item;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void PlainText_AddRange(ListFast<char> items, int count)
-    {
-        PlainText_EnsureCapacity(_plainText_Count + count);
-        // We usually add small enough arrays that a loop is faster
-        for (int i = 0; i < count; i++)
-        {
-            _plainText[_plainText_Count + i] = items[i];
-        }
-        _plainText_Count += count;
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void PlainText_EnsureCapacity(int min)
-    {
-        if (_plainText_Capacity >= min) return;
-        PlainText_Grow(min);
-    }
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private void PlainText_Grow(int min)
-    {
-        int newCapacity = _plainText_Capacity == 0 ? 4 : _plainText_Capacity * 2;
-        if ((uint)newCapacity > 2146435071U) newCapacity = 2146435071;
-        if (newCapacity < min) newCapacity = min;
-        SetPlainTextCapacity(newCapacity);
-    }
-
-    private void SetPlainTextCapacity(int value)
-    {
-        if (value == _plainText_Capacity) return;
-        if (value > 0)
-        {
-            char[] objArray = new char[value];
-            if (_plainText_Count > 0) Array.Copy(_plainText, 0, objArray, 0, _plainText_Count);
-            _plainText = objArray;
-            _plainText_Capacity = value;
-            if (_plainText_Capacity < _plainText_Count)
-            {
-                _plainText_Count = _plainText_Capacity;
-            }
-        }
-        else
-        {
-            _plainText = Array.Empty<char>();
-            _plainText_Capacity = 0;
-            _plainText_Count = 0;
-        }
-    }
-
-    private void PlainText_HardReset(int capacity)
-    {
-        _plainText_Count = 0;
-        SetPlainTextCapacity(capacity);
     }
 
     #endregion
---
+++ ReasonableRTF/_GrowableArray_PlainText.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.CompilerServices;
+using ReasonableRTF.Models.DataTypes;
+
+namespace ReasonableRTF;
+
+public sealed partial class RtfToTextConverter
+{
+    private const int _plainTextDefaultCapacity = 4096;
+
+    private char[] _plainText = new char[_plainTextDefaultCapacity];
+    private int _plainText_Capacity;
+
+    private int _plainText_Count;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void PlainText_Add(char item)
+    {
+        if (_plainText_Count == _plainText_Capacity)
+        {
+            PlainText_EnsureCapacity(_plainText_Count + 1);
+        }
+        _plainText[_plainText_Count++] = item;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void PlainText_AddRange(ListFast<char> items, int count)
+    {
+        PlainText_EnsureCapacity(_plainText_Count + count);
+        // We usually add small enough arrays that a loop is faster
+        for (int i = 0; i < count; i++)
+        {
+            _plainText[_plainText_Count + i] = items[i];
+        }
+        _plainText_Count += count;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void PlainText_EnsureCapacity(int min)
+    {
+        if (_plainText_Capacity >= min) return;
+        PlainText_Grow(min);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void PlainText_Grow(int min)
+    {
+        int newCapacity = _plainText_Capacity == 0 ? 4 : _plainText_Capacity * 2;
+        if ((uint)newCapacity > 2146435071U) newCapacity = 2146435071;
+        if (newCapacity < min) newCapacity = min;
+        SetPlainTextCapacity(newCapacity);
+    }
+
+    private void SetPlainTextCapacity(int value)
+    {
+        if (value == _plainText_Capacity) return;
+        if (value > 0)
+        {
+            char[] objArray = new char[value];
+            if (_plainText_Count > 0) Array.Copy(_plainText, 0, objArray, 0, _plainText_Count);
+            _plainText = objArray;
+            _plainText_Capacity = value;
+            if (_plainText_Capacity < _plainText_Count)
+            {
+                _plainText_Count = _plainText_Capacity;
+            }
+        }
+        else
+        {
+            _plainText = Array.Empty<char>();
+            _plainText_Capacity = 0;
+            _plainText_Count = 0;
+        }
+    }
+
+    private void PlainText_HardReset(int capacity)
+    {
+        _plainText_Count = 0;
+        SetPlainTextCapacity(capacity);
+    }
+}
