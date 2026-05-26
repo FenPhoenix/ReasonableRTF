f7609756568e2bb8f0c2f559e1f6649be25fbc30
FenPhoenix <FenPhoenix@users.noreply.github.com>
2026-05-26 16:48:21 -0700

    Add note for future reference to answer a question I've been wondering about

--- ReasonableRTF/RtfToTextConverter.cs
+++ ReasonableRTF/RtfToTextConverter.cs
@@ -4153,6 +4153,9 @@ public sealed partial class RtfToTextConverter
         And, yes, that's last used, period, regardless of group. So we track it globally. That's the official
         behavior, don't ask me.
 
+        Also, "used" means "acted on", so fonts that were set in skipped destinations don't count, tested and
+        confirmed.
+
         Verified, these 0xF020-0xF0FF chars can be represented either as negatives or as >32767 positives
         (despite the spec saying that \uN must be signed int16). So we need to fall through to this section
         even if we did the above, because by adding 65536 we might now be in the 0xF020-0xF0FF range.
